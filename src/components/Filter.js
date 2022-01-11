bada832cb67c83ab3590778011a5e1185655fad5
Quang <s3817788@rmit.edu.vn>
2022-01-11 11:26:37 +0700

    Fix bug

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -55,6 +55,7 @@ export const Filter = ({
         fetch(endPointCourse)
             .then((response) => response.json())
             .then(async (fetchResult) => {
+                setCheckLoaded(true);
                 fetched = fetchResult.hits;
                 if (fetchResult._links.next === undefined) {
                     coursesIn([]);
@@ -220,7 +221,7 @@ export const Filter = ({
                         count++;
                     }
                     console.log(combination);
-                    let course8Array = combination.splice(0,10)
+                    let course8Array = combination.splice(0, 10);
                     console.log(course8Array);
                     coursesIn(course8Array);
                     if (0 == fetched.length) {
